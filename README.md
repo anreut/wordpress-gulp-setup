f4252aa022922cd481003f8e252c12ac907a8829
anreut <nastassia.reut@gmail.com>
2019-07-14 18:30:16 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,100 @@
+#Build WordPress themes with Gulp
+
+This is a boilerplate implementation of Gulp for WordPress. I've created it for personal use, but feel free to use it with your projects.
+
+###What's inside?
+- [Gulp 4](https://gulpjs.com/) - Task runner 
+- [Sass](https://sass-lang.com/) - a CSS preprocessor
+- [Rollup](https://rollupjs.org/guide/en/) - a JavaScript Modules Bundler
+- [Babel (soon)](https://babeljs.io/) - JavaScript ES6 to ES5 compiler
+
+###Installation
+Clone or download the files and place it to root directory of the theme.
+```
+theme-name
+|   package.json
+|   gulpfile.babel.js
+|   .babelrc  
+|   .gitignore
+|
+└───src
+|   └───images
+|   └───js
+|   └───sass  
+|
+|   style.css
+|   index.php
+|   ... other php files
+```
+In `gulpfile.babel.js` file change proxy address
+```javascript
+export const serve = (done) => {
+    server.init({
+        // change this address
+        proxy: 'http://localhost:8888/your-wordpress-theme/'
+    });
+    done();
+};
+```
+
+###Development
+Gulp is used to compile and optimize theme's scripts, stylesheets, and images.
+Before being able to build theme you have to resolve required dependencies.
+
+```bash
+# @ wp-content/themes/<theme-name>
+
+# Install node dependencies.
+$ npm install
+```
+Now you have all the packages necessary to run the build process and start developing your theme.
+
+There are two available commands:
+```bash
+# @ wp-content/themes/<theme-name>
+
+# Development mode: compiles unminified and unoptimized theme assets with source maps.
+# Init watch mode.
+$ npm run start:dev
+
+# Production build: compiles minified and optimized theme assets
+$ npm run build
+```
+
+###Theme structure
+After you run Gulp development mode it creates /dest directory with all compiled assets. Make sure to serve your static assets from this directory (not /src directory).
+
+Your WordPress theme structure at this step should look like below:
+```
+theme-name
+|   package.json
+|   gulpfile.babel.js
+|   .babelrc  
+|   .gitignore
+|
+└───dest
+|   └───images
+|   └───js
+|   └───css
+
+└───src
+|   └───images
+|   └───js
+|   └───sass  
+|
+|   style.css
+|   index.php
+|   ... other php files
+```
+
+###Notes
+I keep main `style.css` in the root directory of the theme with a header comment section only.
+Other styles I keep in /src/sass and then serve from /dest/css directory. 
+
+Happy coding! 😎
+
+
+
+
+
+
---
+++ gulpfile.babel.js
@@ -0,0 +1,112 @@
+import gulp from 'gulp';
+import yargs from 'yargs';
+import sass from 'gulp-sass';
+import cleanCSS from 'gulp-clean-css';
+import postcss from 'gulp-postcss';
+import autoprefixer from 'autoprefixer';
+import gulpif from 'gulp-if';
+import sourcemaps from 'gulp-sourcemaps';
+import imagemin from 'gulp-imagemin';
+import del from 'del';
+import uglify from 'gulp-uglify-es';
+import rollup from 'gulp-rollup';
+import resolve from 'rollup-plugin-node-resolve';
+import browserSync from 'browser-sync';
+
+const server = browserSync.create();
+const PRODUCTION = yargs.argv.prod;
+
+const paths = {
+    styles: {
+        src: './src/sass/bundle.scss',
+        dest: './dest/css'
+    },
+    images: {
+        src: './src/images/**/*.{jpeg,jpg,png,svg,gif}',
+        dest: './dest/images'
+    },
+    scripts: {
+        src: './src/js/bundle.js',
+        dest: './dest/js'
+    },
+    other: {
+        src: ['./src/**/*', '!./src/{images,js,sass}', '!./src/{images,js,sass}/**/*'],
+        dest: './dest'
+    }
+};
+
+export const serve = (done) => {
+    server.init({
+        proxy: 'http://localhost:8888/your-wordpress-theme/'
+    });
+    done();
+};
+
+export const reload = (done) => {
+    server.reload();
+    done();
+};
+
+export const clean = () => {
+    return del(['./dest'], {force: true});
+};
+
+export const styles = () => {
+    return gulp.src(paths.styles.src)
+        .pipe(gulpif(!PRODUCTION, sourcemaps.init()))
+        .pipe(sass().on('error', sass.logError))
+        .pipe(postcss([ autoprefixer() ]))
+        .pipe(gulpif(PRODUCTION, cleanCSS({compatibility: 'ie8'})))
+        .pipe(gulpif(!PRODUCTION, sourcemaps.write()))
+        .pipe(gulp.dest(paths.styles.dest))
+};
+
+export const scripts = () => {
+    return gulp.src('./src/js/**/*.js')
+        .pipe(gulpif(!PRODUCTION, sourcemaps.init()))
+        .pipe(rollup({
+            allowRealFiles: true,
+            format: 'iife',
+            input: [paths.scripts.src],
+            plugins: [
+                resolve({
+                    main: true,
+                    browser: true
+                })
+            ]
+        }))
+        .pipe(gulpif(PRODUCTION, uglify()))
+        .pipe(gulpif(!PRODUCTION, sourcemaps.write()))
+        .pipe(gulp.dest(paths.scripts.dest))
+};
+
+export const images = () => {
+    return gulp.src(paths.images.src)
+        .pipe(gulpif(PRODUCTION, imagemin()))
+        .pipe(gulp.dest(paths.images.dest))
+};
+
+export const copy = () => {
+    return gulp.src(paths.other.src)
+        .pipe(gulp.dest(paths.other.dest))
+};
+
+export const watch = () => {
+    gulp.watch('./src/sass/**/*.scss', gulp.series(styles, reload));
+    gulp.watch(paths.images.src, gulp.series(images, reload));
+    gulp.watch(paths.other.src, gulp.series(copy, reload));
+    gulp.watch('./src/js/**/*.js', gulp.series(scripts, reload));
+    gulp.watch('../**/*.php', reload)
+};
+
+// Build files for development (with watch mode)
+export const dev = (done) => {
+    return gulp.series(clean, gulp.parallel(styles, styles, images, copy, scripts), serve, watch)(done);
+};
+
+// Build files for production (without watch mode)
+export const build = (done) => {
+    return gulp.series(clean, gulp.parallel(styles, styles, images, copy, scripts))(done);
+};
+
+export default dev;
